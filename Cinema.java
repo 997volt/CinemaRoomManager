25b22da2c187f0b0d81db4efea89f79fb00da236
997volt <p.halaburdzin@gmail.com>
2023-02-17 20:19:49 +0100

    refactor code

--- Cinema.java
+++ Cinema.java
@@ -1,22 +1,72 @@
 package cinema;
 
-public class Cinema {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Cinema {
+    private int rowsCount = 0;
+    private int seatsInRowCount = 0;
+    private final int totalSeatsNumber;
+    private char[][] seatsArray;
+
+
+    public Cinema() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        rowsCount = in.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        seatsInRowCount = in.nextInt();
+        totalSeatsNumber = rowsCount * seatsInRowCount;
+        seatsArray = new char[rowsCount][seatsInRowCount];
+        for (int row = 0; row < rowsCount; row++){
+            for (int seat = 0; seat < seatsInRowCount; seat++) {
+                seatsArray[row][seat] = 'S';
+            }
+        }
+    }
+
+    public void printCinemaLayout() {
         System.out.println("Cinema:");
-        for (int row = 0; row < 8; row++){
-            for (int column = 0; column < 9; column++){
-                if (row == 0 && column == 0){
+        for (int row = 0; row < rowsCount + 1; row++){
+            for (int seat = 0; seat < seatsInRowCount + 1; seat++){
+                if (row == 0 && seat == 0){
                     System.out.print("  ");
                 } else if (row == 0){
-                    System.out.print(Integer.toString(column) + ' ');
-                } else if (column == 0) {
+                    System.out.print(Integer.toString(seat) + ' ');
+                } else if (seat == 0) {
                     System.out.print(Integer.toString(row) + ' ');
                 } else {
-                    System.out.print("S ");
+                    char seatState = seatsArray[row-1][seat-1];
+                    System.out.print(Character.toString(seatState) + ' ');
                 }
             }
             System.out.print("\n");
         }
     }
+
+    private int getSeatPrice(int row) {
+        int price = 0;
+        if (totalSeatsNumber > 60 && row > rowsCount / 2){
+            price = 8;
+        } else {
+            price = 10;
+        }
+        return price;
+    }
+
+
+    // exercise 2 solution
+    private int calculateProfit(){
+        int profit = 0;
+
+        for (int row = 1; row <= rowsCount; row++){
+            profit += getSeatPrice(row) * seatsInRowCount;
+        }
+        return profit;
+    }
+
+    public void printProfit(){
+        System.out.println("Total income:");
+        System.out.println("$" + Integer.toString(calculateProfit()));
+    }
+
 }
